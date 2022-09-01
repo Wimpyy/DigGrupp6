3f6a45c2c8688e9032be0a20fce901c420a4cd14
Rosengren251 <mans.rosengren@elev.ga.lbs.se>
2022-09-01 10:46:59 +0200

    Input system

---
+++ DigGrupp6/Assets/MANS/InputManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour, Controls.IPlayerActions
+{
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
