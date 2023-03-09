35a4ecc97bb50e1deb143593dc03958a681f7178
Cumty <B2yanyan72@hotmail.com>
2023-03-09 09:01:32 +0100

    Grahh Graah

--- DigGrupp6/Assets/YANNICK/Scripts/HealthPickup.cs
+++ DigGrupp6/Assets/YANNICK/Scripts/HealthPickup.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
-    public int healthGainValue = 5;
+    ParticleSystem particleSyst;
 
-    public YoinkerScript yoinker;
+    List<ParticleSystem.Particle> particles = new List<ParticleSystem.Particle>();
+
+    public int healthPerParticle = 1;
+
+    float particleCounter = 0;
+    float particleCountPP = 0.01f;
 
     private void Start()
     {
-        yoinker = FindObjectOfType<YoinkerScript>();
-
+        particleSyst = GetComponent<ParticleSystem>();
     }
 
-    
+    private void OnParticleTrigger()
+    {
+        int triggerParticles = particleSyst.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
+
+        for (int i = 0; i < triggerParticles; i++)
+        {
+            ParticleSystem.Particle p = particles[i];
+            p.remainingLifetime = 0;
+            if (FindObjectOfType<PlayerLifeSupport>().ifHearts)
+            {
+                Debug.Log("particle Check start");
+                particleCounter += particleCountPP;
+                if (particleCounter >= 1)
+                {
+                    FindObjectOfType<PlayerLifeSupport>().GainHealth(healthPerParticle);
+                    particleCounter = 0;
+                    Debug.Log("healthHearts Gained");
+                }
+            }
+            else 
+            {
+                FindObjectOfType<PlayerLifeSupport>().GainHealth(healthPerParticle);
+                Debug.Log("Healthbar Gained");
+            }
+            particles[i] = p;
+        }
+        particleSyst.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
+    }
 }
